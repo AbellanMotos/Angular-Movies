1aa05024a8bfded81324213b3cc6e74f90256629
Jose <jaabellanmotos@gmail.com>
2019-07-18 19:38:32 +0200

    added stars & homepage + styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MoviesCategoryComponent } from './movies-category/movies-category.component';
 import { MoviesApiService } from './services/movies-api.service';
-import { ListMoviesComponent } from './list-movies/list-movies.component'
+import { ListMoviesComponent } from './list-movies/list-movies.component';
 
+import { NgxStarsModule } from 'ngx-stars';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { MoviesSearchComponent } from './movies-search/movies-search.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
@@ -31,6 +32,7 @@ import { MoviesGenresComponent } from './movies-genres/movies-genres.component';
     HttpClientModule,
     InfiniteScrollModule,
     FormsModule,
+    NgxStarsModule,
   ],
   providers: [MoviesApiService],
   bootstrap: [AppComponent]
